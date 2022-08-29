bc390ee6708c0e49a9c0d967727a551f75dd8e58
Anton Vanyukov <anton@incsub.com>
2022-08-29 18:06:12 +1000

    Fix layout issues

--- App/Views/Settings.php
+++ App/Views/Settings.php
@@ -30,64 +30,63 @@ $stats = get_option( 'cf-images-stats', array( 'synced' => 0 ) );
 			<div id="post-body-content">
 				<div class="postbox">
 					<h2><?php esc_html_e( 'Settings', 'cf-images' ); ?></h2>
-					<div class="inside">
-						<form id="cf-images-form" data-type="settings">
-							<table class="form-table" role="presentation">
-								<tbody>
-								<tr>
-									<th scope="row">
-										<?php esc_html_e( 'Serve from custom domain', 'cf-images' ); ?>
-									</th>
-									<td>
-										<fieldset>
-											<legend class="screen-reader-text">
-												<span><?php esc_html_e( 'Serve images from custom domains', 'cf-images' ); ?></span>
-											</legend>
-											<label for="custom_domain">
-												<input name="custom-domain" type="checkbox" id="custom_domain" value="1" <?php checked( get_option( 'cf-images-custom-domain', false ) ); ?>>
-												<?php esc_html_e( 'Enable', 'cf-images' ); ?>
-											</label>
-										</fieldset>
-										<p class="description">
-											<?php esc_html_e( 'Use the current site domain instead of `imagedelivery.net`.', 'cf-images' ); ?>
-										</p>
-										<p class="description">
-											<?php esc_html_e( 'Note: Image delivery is supported from all customer domains under the same Cloudflare account.', 'cf-images' ); ?>
-										</p>
-									</td>
-								</tr>
 
-								<tr>
-									<th scope="row">
-										<?php esc_html_e( 'Disable WordPress image sizes', 'cf-images' ); ?>
-									</th>
-									<td>
-										<fieldset>
-											<legend class="screen-reader-text">
-												<span><?php esc_html_e( 'Disable WordPress image sizes', 'cf-images' ); ?></span>
-											</legend>
-											<label for="disable_sizes">
-												<input name="disable-sizes" type="checkbox" id="disable_sizes" value="1" <?php checked( get_option( 'cf-images-disable-generation', false ) ); ?>>
-												<?php esc_html_e( 'Enable', 'cf-images' ); ?>
-											</label>
-										</fieldset>
-										<p class="description">
-											<?php esc_html_e( 'Setting this option will disable generation of `-scaled` images and other image sizes. Only the original image will be stored in the media library. Only for newly uploaded files, current images will not be affected.', 'cf-images' ); ?>
-										</p>
-										<p class="description">
-											<?php esc_html_e( 'Note: This feature is experimental. All the image sizes can be restored with the `Regenerate Thumbnails` plugin.', 'cf-images' ); ?>
-										</p>
-									</td>
-								</tr>
-								</tbody>
-							</table>
+					<form class="inside" id="cf-images-form" data-type="settings">
+						<table class="form-table" role="presentation">
+							<tbody>
+							<tr>
+								<th scope="row">
+									<?php esc_html_e( 'Serve from custom domain', 'cf-images' ); ?>
+								</th>
+								<td>
+									<fieldset>
+										<legend class="screen-reader-text">
+											<span><?php esc_html_e( 'Serve images from custom domains', 'cf-images' ); ?></span>
+										</legend>
+										<label for="custom_domain">
+											<input name="custom-domain" type="checkbox" id="custom_domain" value="1" <?php checked( get_option( 'cf-images-custom-domain', false ) ); ?>>
+											<?php esc_html_e( 'Enable', 'cf-images' ); ?>
+										</label>
+									</fieldset>
+									<p class="description">
+										<?php esc_html_e( 'Use the current site domain instead of `imagedelivery.net`.', 'cf-images' ); ?>
+									</p>
+									<p class="description">
+										<?php esc_html_e( 'Note: Image delivery is supported from all customer domains under the same Cloudflare account.', 'cf-images' ); ?>
+									</p>
+								</td>
+							</tr>
 
-							<p>
-								<input type="submit" name="submit" class="button button-primary" value="<?php esc_attr_e( 'Save Changes', 'cf-images' ); ?>">
-								<span class="spinner"></span>
-							</p>
-						</form>
-					</div>
+							<tr>
+								<th scope="row">
+									<?php esc_html_e( 'Disable WordPress image sizes', 'cf-images' ); ?>
+								</th>
+								<td>
+									<fieldset>
+										<legend class="screen-reader-text">
+											<span><?php esc_html_e( 'Disable WordPress image sizes', 'cf-images' ); ?></span>
+										</legend>
+										<label for="disable_sizes">
+											<input name="disable-sizes" type="checkbox" id="disable_sizes" value="1" <?php checked( get_option( 'cf-images-disable-generation', false ) ); ?>>
+											<?php esc_html_e( 'Enable', 'cf-images' ); ?>
+										</label>
+									</fieldset>
+									<p class="description">
+										<?php esc_html_e( 'Setting this option will disable generation of `-scaled` images and other image sizes. Only the original image will be stored in the media library. Only for newly uploaded files, current images will not be affected.', 'cf-images' ); ?>
+									</p>
+									<p class="description">
+										<?php esc_html_e( 'Note: This feature is experimental. All the image sizes can be restored with the `Regenerate Thumbnails` plugin.', 'cf-images' ); ?>
+									</p>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+
+						<p>
+							<input type="submit" name="submit" class="button button-primary" value="<?php esc_attr_e( 'Save Changes', 'cf-images' ); ?>">
+							<span class="spinner"></span>
+						</p>
+					</form>
 				</div>
 			</div>
 
