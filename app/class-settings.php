0d108b3ba54db680c05f6b75af837d595602db59
Anton Vanyukov <a.vanyukov@vcore.ru>
2023-03-18 11:57:56 +1000

    Increase timeout to 2 seconds

--- app/class-settings.php
+++ app/class-settings.php
@@ -158,7 +158,7 @@ class Settings {
 		update_option( 'cf-images-config-written', ! empty( $value ), false );
 
 		// On some hosts, the wp-config.php updates take several seconds to "un-cache".
-		sleep( 1 );
+		sleep( 2 );
 
 	}
 
