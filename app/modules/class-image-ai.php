a1fdaa183505c3d4e048af39d2fd5ca96e1646f7
Anton Vanyukov <a.vanyukov@vcore.ru>
2023-06-26 17:17:21 +1000

    Save work in progress

---
+++ app/modules/class-image-ai.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Image AI module
+ *
+ * Add AI-based functionality for tagging and captioning images.
+ *
+ * @link https://vcore.au
+ *
+ * @package CF_Images
+ * @subpackage CF_Images/App/Modules
+ * @author Anton Vanyukov <a.vanyukov@vcore.ru>
+ *
+ * @since 1.4.0
+ */
+
+namespace CF_Images\App\Modules;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Image_Ai class.
+ *
+ * @since 1.4.0
+ */
+class Image_Ai extends Module {
+
+	/**
+	 * Init the module.
+	 *
+	 * @since 1.4.0
+	 */
+	public function init() {
+
+	}
+
+}
