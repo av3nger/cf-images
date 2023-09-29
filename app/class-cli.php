71471e77de778be515ccb49223234f0c4c0f312c
Anton Vanyukov <a.vanyukov@vcore.ru>
2023-09-29 12:16:00 +1000

    Add CLI class

---
+++ app/class-cli.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WP CLI functionality
+ *
+ * @link https://vcore.au
+ *
+ * @package CF_Images
+ * @subpackage CF_Images/App
+ * @author Anton Vanyukov <a.vanyukov@vcore.ru>
+ * @since 1.5.0
+ */
+
+namespace CF_Images\App;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * CLI class.
+ *
+ * @since 1.5.0
+ */
+class CLI {
+
+}
