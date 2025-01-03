f9a12ab0c6b6127504d1872373e7bdb37ccf9d20
Anton Vanyukov <a.vanyukov@vcore.ru>
2025-01-03 11:20:41 +1000

    Fix phpcs warnings

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,7 +18,7 @@ if ( false !== $_phpunit_polyfills_path ) {
 }
 
 if ( ! file_exists( "$_tests_dir/includes/functions.php" ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?".PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
 }
 
